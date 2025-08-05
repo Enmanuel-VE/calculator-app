70f67ba9c3264456a1e94d376c1ac17cd878cd9d
Enmanuel Antoni Bracho Villanueva <enmanuel02estudios@gmail.com>
2025-08-05 16:46:45 -0400

    fix: set correct base path for GitHub Pages in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [],
-	base: "https://Enmanuel-VE.github.io/calculator-app",
+	base: "/calculator-app/",
 });
