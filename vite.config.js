e3902c1b3df467b408db588b6d91b98cfba0921f
Enmanuel Antoni Bracho Villanueva <enmanuel02estudios@gmail.com>
2025-08-05 17:08:42 -0400

    feat: test of diploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [],
-	base: "/calculator-app/",
+	base: "https://Enmanuel-VE.github.io/calculator-app/",
 });
