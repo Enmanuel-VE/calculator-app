c3fb30f2b684e4bc7766d2dcd68623148162565f
Enmanuel Antoni Bracho Villanueva <enmanuel02estudios@gmail.com>
2025-08-07 01:06:51 -0400

    Updates

---
+++ assets/HistoryPage-Bi_jvDxw.js
@@ -0,0 +1,1 @@
+const e=()=>{const t=document.createElement("div");return t.textContent="< HistoryPage />",t};export{e as default};
---
+++ assets/NotFoundPage-CaIccL_W.js
@@ -0,0 +1,1 @@
+const e=()=>{const n=document.createElement("div");return n.innerHTML="<h1>404 - Página no encontrada</h1>",n};export{e as default};
